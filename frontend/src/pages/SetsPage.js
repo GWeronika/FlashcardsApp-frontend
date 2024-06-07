16844d6c5c6996204d8428ffcb155d4ab918dad8
weronika <weronika.gawel@onet.eu>
2024-06-07 19:00:55 +0200

    Sets display

--- frontend/src/pages/SetsPage.js
+++ frontend/src/pages/SetsPage.js
@@ -1,13 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/SetsPage.css';
 
 const SetsPage = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [sets, setSets] = useState([]);
 
-    // TODO: pobieranie zbiorów z bazy danych (do zaimplementowania)
+    useEffect(() => {
+        fetchSets();
+    }, []);
 
-    //TODO: obsługa zmiany w polu wyszukiwania
+    const fetchSets = async () => {
+        try {
+            const response = await fetch('/api/set/select/all', {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' },
+            });
+            if (response.ok) {
+                const setsData = await response.json();
+                setSets(setsData);
+            } else {
+                console.error('Failed to fetch sets');
+            }
+        } catch (error) {
+            console.error('Error fetching sets:', error);
+        }
+    };
+
     const handleSearchChange = (event) => {
         setSearchTerm(event.target.value);
         //TODO: filtrowanie wyników
@@ -26,20 +44,20 @@ const SetsPage = () => {
                     <i className="fa-solid fa-magnifying-glass"></i></button>
             </div>
             <div className="sets-div">
-                {/*{sets.map((set) => (*/}
-                {/*    <div key={set.id} className="set-box">*/}
-                {/*        <div className="set-title">{set.title}</div>*/}
-                {/*        <div className="set-description">{set.description}</div>*/}
-                {/*        <div className="flashcards">*/}
-                {/*            {set.flashcards.map((flashcard, index) => (*/}
-                {/*                <div key={index} className="flashcard">*/}
-                {/*                    <div>{flashcard.question}</div>*/}
-                {/*                    <div>{flashcard.answer}</div>*/}
-                {/*                </div>*/}
-                {/*            ))}*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*))}*/}
+                {sets.map((set) => (
+                    <div key={set.setId} className="set-box">
+                        <div className="set-title">{set.name}</div>
+                        <div className="set-description">{set.description}</div>
+                        {/*<div className="flashcards">*/}
+                        {/*    {set.flashcards.map((flashcard, index) => (*/}
+                        {/*        <div key={index} className="flashcard">*/}
+                        {/*            <div>{flashcard.word}</div>*/}
+                        {/*            <div>{flashcard.description}</div>*/}
+                        {/*        </div>*/}
+                        {/*    ))}*/}
+                        {/*</div>*/}
+                    </div>
+                ))}
             </div>
         </div>
     );
