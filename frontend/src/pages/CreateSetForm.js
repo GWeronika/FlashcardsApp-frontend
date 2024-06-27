50b91ba9bba1d820165da010604657b14212bc45
weronika <weronika.gawel@onet.eu>
2024-06-27 19:03:02 +0200

    Delete/update flashcard

--- frontend/src/pages/CreateSetForm.js
+++ frontend/src/pages/CreateSetForm.js
@@ -12,6 +12,8 @@ const CreateSetForm = ({ isLoggedIn, currentUser, onRedirect, onRedirectToSetsPa
     const [isModalOpen, setIsModalOpen] = useState(true);
     const [setObject, setSetObject] = useState(null);
     const [flashcards, setFlashcards] = useState([]);
+    const [editingFlashcard, setEditingFlashcard] = useState(null);
+    const [, setIsEditingModalOpen] = useState(false);
 
     useEffect(() => {
         if (!isLoggedIn) {
@@ -31,12 +33,12 @@ const CreateSetForm = ({ isLoggedIn, currentUser, onRedirect, onRedirectToSetsPa
     };
 
     const handleSetDescriptionChange = (e) => {
-        const { value } = e.target;
+        const {value} = e.target;
         setSetDescription(value);
     };
 
     const handleSetWordChange = (e) => {
-        const { value } = e.target;
+        const {value} = e.target;
         setNewFlashcard(prevState => ({
             ...prevState,
             word: value
@@ -44,15 +46,14 @@ const CreateSetForm = ({ isLoggedIn, currentUser, onRedirect, onRedirectToSetsPa
     };
 
     const handleSetDefinitionChange = (e) => {
-        const { value } = e.target;
+        const {value} = e.target;
         setNewFlashcard(prevState => ({
             ...prevState,
             description: value
         }));
     };
 
-    const handleAddFlashcard = async (e) => {
-        e.preventDefault();
+    const handleAddFlashcard = async () => {
         if (!setObject) {
             alert("Set object is not available. Create a set first.");
             return;
@@ -65,13 +66,13 @@ const CreateSetForm = ({ isLoggedIn, currentUser, onRedirect, onRedirectToSetsPa
         try {
             const response = await fetch(`/api/flashcard/add?${params.toString()}`, {
                 method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
+                headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify(setObject)
             });
             if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
+                console.error(`HTTP error! status: ${response.status}`);
             }
-            setNewFlashcard({ word: '', description: '' });
+            setNewFlashcard({word: '', description: ''});
 
             await fetchFlashcardsBySetId(setObject.setId);
         } catch (error) {
@@ -95,7 +96,7 @@ const CreateSetForm = ({ isLoggedIn, currentUser, onRedirect, onRedirectToSetsPa
                     body: params.toString()
                 });
                 if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
+                    console.error(`HTTP error! status: ${response.status}`);
                 }
                 const result = await response.json();
                 setSetObject(result);
@@ -111,7 +112,7 @@ const CreateSetForm = ({ isLoggedIn, currentUser, onRedirect, onRedirectToSetsPa
         try {
             const response = await fetch(`/api/flashcard/select/setid?setId=${setObject.setId}`);
             if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
+                console.error(`HTTP error! status: ${response.status}`);
             }
             const flashcardsData = await response.json();
 
@@ -130,10 +131,10 @@ const CreateSetForm = ({ isLoggedIn, currentUser, onRedirect, onRedirectToSetsPa
         try {
             const response = await fetch(`/api/set/delete?id=${setObject.setId}`, {
                 method: 'DELETE',
-                headers: { 'Content-Type': 'application/json' }
+                headers: {'Content-Type': 'application/json'}
             });
             if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
+                console.error(`HTTP error! status: ${response.status}`);
             }
             setSetObject(null);
             setSetName('');
@@ -145,28 +146,61 @@ const CreateSetForm = ({ isLoggedIn, currentUser, onRedirect, onRedirectToSetsPa
 
     const handleDeleteButtonClick = async () => {
         try {
+            onRedirectToSetsPage();
             await handleDeleteFlashcardsInSet();
             await handleDeleteSet();
-            onRedirectToSetsPage();
         } catch (error) {
             console.error(`Failed to delete set and flashcards: ${error.message}`);
         }
     };
 
     const handleConfirmFlashcard = async () => {
-        await handleAddFlashcard();
-        alert("Successfully added");
+        await updateSetName();
+        await updateSetDescription();
+        alert("Successfully added and updated");
         onRedirectToSetsPage();
+    };
+
+    const updateSetName = async () => {
+        if (setName.trim() !== '' && setObject) {
+            try {
+                const response = await fetch(`/api/set/edit?id=${setObject.setId}&newName=${setName}`, {
+                    method: 'GET',
+                    headers: {'Content-Type': 'application/json'}
+                });
+                if (!response.ok) {
+                    console.error(`HTTP error! status: ${response.status}`);
+                }
+            } catch (error) {
+                alert(`Failed to update set name: ${error.message}`);
+            }
+        }
+    };
+
+    const updateSetDescription = async () => {
+        if (setDescription.trim() !== '' && setObject) {
+            try {
+                const response = await fetch(`/api/set/edit/description?id=${setObject.setId}&description=${setDescription}`, {
+                    method: 'GET',
+                    headers: {'Content-Type': 'application/json'}
+                });
+                if (!response.ok) {
+                    console.error(`HTTP error! status: ${response.status}`);
+                }
+            } catch (error) {
+                alert(`Failed to update set description: ${error.message}`);
+            }
+        }
     };
 
     const handleDeleteFlashcard = async (flashcardId) => {
         try {
             const response = await fetch(`/api/flashcard/delete?id=${flashcardId}`, {
                 method: 'DELETE',
-                headers: { 'Content-Type': 'application/json' }
+                headers: {'Content-Type': 'application/json'}
             });
             if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
+                console.error(`HTTP error! status: ${response.status}`);
             }
 
             await fetchFlashcardsBySetId(setObject.setId);
@@ -179,7 +213,7 @@ const CreateSetForm = ({ isLoggedIn, currentUser, onRedirect, onRedirectToSetsPa
         try {
             const response = await fetch(`/api/flashcard/select/setid?setId=${setId}`);
             if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
+                console.error(`HTTP error! status: ${response.status}`);
             }
             const flashcardsData = await response.json();
             setFlashcards(flashcardsData);
@@ -188,82 +222,136 @@ const CreateSetForm = ({ isLoggedIn, currentUser, onRedirect, onRedirectToSetsPa
         }
     };
 
+    const handleUpdateFlashcardConfirm = async () => {
+        try {
+            const {word, description} = editingFlashcard;
+            const response = await fetch(`/api/flashcard/edit?id=${editingFlashcard.flashcardId}&word=${word}&description=${description}`, {
+                method: 'PUT',
+                headers: {'Content-Type': 'application/json'}
+            });
+            if (!response.ok) {
+                console.error(`HTTP error! status: ${response.status}`);
+            }
+            alert("Flashcard updated successfully");
+            setEditingFlashcard(null);
+            await fetchFlashcardsBySetId(setObject.setId);
+        } catch (error) {
+            alert(`Failed to update flashcard: ${error.message}`);
+        }
+    };
+
+    const openEditModal = (flashcard) => {
+        setEditingFlashcard(flashcard);
+        setIsEditingModalOpen(true);
+    };
+
+    const closeEditModal = () => {
+        setEditingFlashcard(null);
+        setIsEditingModalOpen(false);
+    };
+
     return (
-        <div className="create-form-container">
-            {isModalOpen && (
-                <div className="modal">
-                    <h2>Create new set</h2>
-                    <input
-                        type="text"
-                        placeholder="Name"
-                        value={setName}
-                        onChange={handleSetNameChange}
-                    />
-                    <textarea
-                        placeholder="Description"
-                        value={setDescription}
-                        onChange={handleSetDescriptionChange}
-                        rows={1}
-                    />
-                    <Button text={<>Create</>} onClick={handleCreateSet} />
+        <>
+            {editingFlashcard && (
+                <div className="modal-overlay">
+                    <div className="change-modal">
+                        <h2>Edit Flashcard</h2>
+                        <input
+                            type="text"
+                            placeholder="Word"
+                            value={editingFlashcard.word}
+                            onChange={(e) => setEditingFlashcard({...editingFlashcard, word: e.target.value})}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Description"
+                            value={editingFlashcard.description}
+                            onChange={(e) => setEditingFlashcard({...editingFlashcard, description: e.target.value})}
+                        />
+                        <div className="modal-buttons">
+                            <Button text={<>Update</>} onClick={handleUpdateFlashcardConfirm}/>
+                            <Button text={<>Cancel</>} onClick={closeEditModal}/>
+                        </div>
+                    </div>
                 </div>
             )}
-            {!isModalOpen && (
-                <>
-                    <div className="create-form-inner">
-                        <div className="name-form">
-                            <div className="name-form-left">
-                            <input
-                                type="text"
-                                placeholder="Set Name"
-                                value={setName}
-                                onChange={handleSetNameChange}
-                            />
-                            <textarea
-                                placeholder="Description"
-                                value={setDescription}
-                                onChange={handleSetDescriptionChange}
-                                rows={3}
-                            />
-                            </div>
-                            <div className="name-form-right">
-                                <i className="fa-solid fa-trash-can" onClick={handleDeleteButtonClick}></i>
-                            </div>
-                        </div>
-                        <div className="create-form">
-                            <input
-                                type="text"
-                                placeholder="Word"
-                                value={newFlashcard.word}
-                                onChange={handleSetWordChange}
-                            />
-                            <input
-                                type="text"
-                                placeholder="Definition"
-                                value={newFlashcard.description}
-                                onChange={handleSetDefinitionChange}
-                            />
-                            <div className="create-form-buttons">
-                                <Button text={<>Add</>} onClick={handleAddFlashcard} />
-                                <Button text={<>Finish</>} onClick={handleConfirmFlashcard} />
-                            </div>
-                        </div>
+            <div className="create-form-container">
+                {isModalOpen && (
+                    <div className="modal">
+                        <h2>Create new set</h2>
+                        <input
+                            type="text"
+                            placeholder="Name"
+                            value={setName}
+                            onChange={handleSetNameChange}
+                        />
+                        <textarea
+                            placeholder="Description"
+                            value={setDescription}
+                            onChange={handleSetDescriptionChange}
+                            rows={1}
+                        />
+                        <Button text={<>Create</>} onClick={handleCreateSet}/>
                     </div>
-                    <div className="flashcard-container">
-                        {flashcards.map((flashcard) => (
-                            <button key={flashcard.flashcardId} className="flashcard">
-                                <h3 className="flashcard-title">{flashcard.word}</h3>
-                                <p className="flashcard-description">{flashcard.description}</p>
-                                <div className="flashcard-options">
-                                    <i className="fa-solid fa-trash-can" onClick={() => handleDeleteFlashcard(flashcard.flashcardId)}></i>
-                                    {/*<i className="fa-solid fa-pen" onClick={() => handleUpdateFlashcard(flashcard)}></i>*/}
+                )}
+                {!isModalOpen && (
+                    <>
+                        <div className="create-form-inner">
+                            <div className="name-form">
+                                <div className="name-form-left">
+                                    <input
+                                        type="text"
+                                        placeholder="Set Name"
+                                        value={setName}
+                                        onChange={handleSetNameChange}
+                                    />
+                                    <textarea
+                                        placeholder="Description"
+                                        value={setDescription}
+                                        onChange={handleSetDescriptionChange}
+                                        rows={3}
+                                    />
                                 </div>
-                            </button>
-                        ))}
-                    </div>
-                </>
-            )}
-        </div>
+                                <div className="name-form-right">
+                                    <i className="fa-solid fa-trash-can" onClick={handleDeleteButtonClick}></i>
+                                </div>
+                            </div>
+                            <div className="create-form">
+                                <input
+                                    type="text"
+                                    placeholder="Word"
+                                    value={newFlashcard.word}
+                                    onChange={handleSetWordChange}
+                                />
+                                <input
+                                    type="text"
+                                    placeholder="Definition"
+                                    value={newFlashcard.description}
+                                    onChange={handleSetDefinitionChange}
+                                />
+                                <div className="create-form-buttons">
+                                    <Button text={<>Add</>} onClick={handleAddFlashcard}/>
+                                    <Button text={<>Finish</>} onClick={handleConfirmFlashcard}/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flashcard-container">
+                            {flashcards.map((flashcard) => (
+                                <div key={flashcard.flashcardId} className="flashcard">
+                                    <h3 className="flashcard-title">{flashcard.word}</h3>
+                                    <p className="flashcard-description">{flashcard.description}</p>
+                                    <div className="flashcard-options">
+                                        <i className="fa-solid fa-trash-can"
+                                           onClick={() => handleDeleteFlashcard(flashcard.flashcardId)}></i>
+                                        <i className="fa-solid fa-pen" onClick={() => openEditModal(flashcard)}></i>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </>
+                )}
+            </div>
+        </>
     );
 };
 
