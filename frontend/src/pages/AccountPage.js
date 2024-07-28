db29ce5149839042a671ebd29734b5116039f2fb
weronika <weronika.gawel@onet.eu>
2024-07-28 10:39:53 +0200

    Edit profile function corrected

--- frontend/src/pages/AccountPage.js
+++ frontend/src/pages/AccountPage.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import UserInfo from './user-account/UserInfo';
 import MySetsBox from './user-account/MySetsBox';
 import '../styles/AccountPage.css';
 
 const AccountPage = ({ user }) => {
+    const [appUser, setAppUser] = useState(user || {});
+
+    const updateUser = (updateUser) => {
+        setAppUser(updateUser);
+    };
+
+    if (!user || !user.userId) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div className="my-account-page">
             <div className="my-account-page-center">
                 <div className="left-column">
-                    <UserInfo user={user} />
+                    <UserInfo user={appUser} updateUser={updateUser} />
                 </div>
                 <div className="right-column">
-                    <MySetsBox user={user} />
+                    <MySetsBox user={appUser} />
                 </div>
             </div>
         </div>
--- frontend/src/pages/user-account/UserInfo.js
+++ frontend/src/pages/user-account/UserInfo.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import EditProfileModal from './EditProfileModal';
+import '../../styles/UserInfo.css';
 
-const UserInfo = ({ user }) => {
+const UserInfo = ({ user, updateUser }) => {
+    const [isModalOpen, setModalOpen] = useState(false);
+
+    const handleOpenModal = () => {
+        setModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setModalOpen(false);
+    };
+
+    const handleSaveProfile = (newUsername, newEmail) => {
+        const updatedUser = { ...user, name: newUsername, email: newEmail };
+        updateUser(updatedUser);
+        handleCloseModal();
+    };
+
+    if (!user || !user.userId) {
+        return <div>Loading user data...</div>;
+    }
+
     return (
         <div className="user-info">
             <h2>{user.name}</h2>
             <p>{user.email}</p>
-            <button className="edit-button">
+            <button className="edit-button" onClick={handleOpenModal}>
                 <i className="fa-solid fa-pen-to-square"></i>
                 Edit profile
             </button>
@@ -13,6 +35,13 @@ const UserInfo = ({ user }) => {
                 <i className="fa-solid fa-key"></i>
                 Change password
             </button>
+            {isModalOpen && (
+                <EditProfileModal
+                    user={user}
+                    onClose={handleCloseModal}
+                    onSave={handleSaveProfile}
+                />
+            )}
         </div>
     );
 };
